1827ff265409ea18c0f77461adc688c9cc955970
Matt <matthew.morley.ca@gmail.com>
2022-09-06 20:58:08 -0400

    dum

--- src/Main_test.cc
+++ src/Main_test.cc
@@ -4,42 +4,47 @@
 #include <wpinet/uv/Timer.h>
 #include <wpinet/WebSocket.h>
 #include <wpinet/uv/Udp.h>
+#include <wpinet/EventLoopRunner.h>
 #include <wpi/Logger.h>
 #include <wpinet/ParallelTcpConnector.h>
 
 namespace uv = wpi::uv;
 
-std::shared_ptr<uv::Loop> loop;
-
-void Finish() {
-  loop->Walk([](uv::Handle& it) { it.Close(); });
+void Finish(uv::Loop& loop) {
+  loop.Walk([](uv::Handle& it) { it.Close(); });
 }
 
 int main() {
-  loop = uv::Loop::Create();
+  wpi::EventLoopRunner runner;
 
-  loop->error.connect(
-      [](uv::Error err) { fmt::print(stderr, "uv ERROR: {}\n", err.str()); });
+  runner.ExecAsync([&](auto& loop) {
+    loop.error.connect(
+        [](uv::Error err) { fmt::print(stderr, "uv ERROR: {}\n", err.str()); });
 
-  auto tcp = uv::Tcp::Create(loop);
-  const char* pipeName = "localhost"; const int port = 9002;
+    auto tcp = uv::Tcp::Create(loop);
+    const char* pipeName = "127.0.0.1";
+    const int port = 9002;
 
-  // Our callback should be called when the TCP connection opens
-  tcp->Connect(pipeName, port, [&] {
-    auto ws = wpi::WebSocket::CreateClient(*tcp, "/test", pipeName);
-    ws->closed.connect([&](uint16_t code, std::string_view reason) {
-      Finish();
-      if (code != 1005 && code != 1006) {
-        // FAIL() << "Code: " << code << " Reason: " << reason;
+    // Our callback should be called when the TCP connection opens
+    tcp->Connect(pipeName, port, [&] {
+      printf("Got TCP connection!\n");
+
+      if (!tcp.get()) {
+        printf("TCP is apparently nullptr?");
       }
-    });
-    ws->open.connect([&](std::string_view protocol) {
-      printf("Got open!\n");
-      Finish();
+      auto ws = wpi::WebSocket::CreateClient(*tcp.get(), "/wpilibws", 
+        fmt::format("{}:{}", pipeName, port));
+      ws->closed.connect([&](uint16_t code, std::string_view reason) {
+        Finish(loop);
+        if (code != 1005 && code != 1006) {
+          // FAIL() << "Code: " << code << " Reason: " << reason;
+        }
+      });
+      ws->open.connect([&](std::string_view protocol) {
+        Finish(loop);
+      });
     });
   });
 
-  printf("Running loop\n");
-  loop->Run();
-  printf("Loop ran!\n");
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
