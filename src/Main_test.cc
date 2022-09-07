4d384c944987460190328a44d9355ca46f9728a4
Matt <matthew.morley.ca@gmail.com>
2022-09-06 21:36:01 -0400

    somewhat works

--- src/Main_test.cc
+++ src/Main_test.cc
@@ -6,7 +6,10 @@
 #include <wpinet/uv/Udp.h>
 #include <wpinet/EventLoopRunner.h>
 #include <wpi/Logger.h>
+#include <wpi/json.h>
 #include <wpinet/ParallelTcpConnector.h>
+#include <wpinet/raw_uv_ostream.h>
+#include <iostream>
 
 namespace uv = wpi::uv;
 
@@ -14,37 +17,42 @@ void Finish(uv::Loop& loop) {
   loop.Walk([](uv::Handle& it) { it.Close(); });
 }
 
+static void SendWsText(std::shared_ptr<wpi::WebSocket> ws, const wpi::json& j) {
+  wpi::SmallVector<uv::Buffer, 4> toSend;
+  wpi::raw_uv_ostream os{toSend, 4096};
+  os << j;
+  printf("sending text\n");
+  ws->SendText(toSend, [](wpi::span<uv::Buffer> bufs, uv::Error) {
+    for (auto&& buf : bufs)
+      buf.Deallocate();
+  });
+}
+
 int main() {
+  printf("Hello!\n");
   wpi::EventLoopRunner runner;
+  std::shared_ptr<uv::Tcp> tcp;
 
   runner.ExecAsync([&](auto& loop) {
     loop.error.connect(
         [](uv::Error err) { fmt::print(stderr, "uv ERROR: {}\n", err.str()); });
 
-    auto tcp = uv::Tcp::Create(loop);
+    tcp = uv::Tcp::Create(loop);
     const char* pipeName = "127.0.0.1";
     const int port = 9002;
 
     // Our callback should be called when the TCP connection opens
     tcp->Connect(pipeName, port, [&] {
       printf("Got TCP connection!\n");
+      auto ws = wpi::WebSocket::CreateClient(
+          *tcp.get(), "", fmt::format("{}:{}", pipeName, port));
 
-      if (!tcp.get()) {
-        printf("TCP is apparently nullptr?");
-      }
-      auto ws = wpi::WebSocket::CreateClient(*tcp.get(), "/wpilibws", 
-        fmt::format("{}:{}", pipeName, port));
-      ws->closed.connect([&](uint16_t code, std::string_view reason) {
-        Finish(loop);
-        if (code != 1005 && code != 1006) {
-          // FAIL() << "Code: " << code << " Reason: " << reason;
-        }
-      });
-      ws->open.connect([&](std::string_view protocol) {
-        Finish(loop);
+      ws->open.connect([&tcp, &ws](std::string_view) {
+        printf("Opened!\n");
+        SendWsText(ws, {{"command", "SET_STATE"}, {"newState", "Foobar"}});
       });
     });
   });
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(10000000));
 }
