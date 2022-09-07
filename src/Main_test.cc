b8ba8338647afc66b50eeca0c47ef1dd8798b91d
Matt <matthew.morley.ca@gmail.com>
2022-09-07 00:11:02 -0400

    ugh

--- src/Main_test.cc
+++ src/Main_test.cc
@@ -2,16 +2,25 @@
 #include <wpinet/uv/Process.h>
 #include <wpinet/uv/Tcp.h>
 #include <wpinet/uv/Timer.h>
+#include <wpinet/WebSocket.h>
 #include <wpinet/uv/Udp.h>
 #include <wpi/Logger.h>
 #include <wpinet/ParallelTcpConnector.h>
 
 namespace uv = wpi::uv;
 
+std::shared_ptr<uv::Loop> loop;
+
+void Finish() {
+  loop->Walk([](uv::Handle& it) { it.Close(); });
+}
+
 int main() {
-  auto loop = uv::Loop::Create();
-  auto tcp = uv::TCP::Create(loop);
-  const auto pipeName = "localhost:9002";
+  loop = uv::Loop::Create();
+  auto tcp = uv::Tcp::Create(loop);
+  const char* pipeName = "localhost:9002";
+
+  // Our callback should be called when the TCP connection opens
   tcp->Connect(pipeName, [&] {
     auto ws = WebSocket::CreateClient(*tcp, "/test", pipeName);
     ws->closed.connect([&](uint16_t code, std::string_view reason) {
