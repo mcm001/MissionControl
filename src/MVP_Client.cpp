a251110dac92332b44f51c657337cdf749bd2d94
Matt M <matthew.morley.ca@gmail.com>
2022-09-08 14:25:19 -0400

    Add MVP server/client which seem to work

---
+++ src/MVP_Client.cpp
@@ -0,0 +1,126 @@
+#include <vector>
+
+#include <wpi/span.h>
+
+#include "wpinet/uv/Loop.h"
+#include "wpinet/uv/Pipe.h"
+#include "wpinet/uv/Tcp.h"
+#include "wpinet/uv/Timer.h"
+
+#include <wpinet/WebSocket.h>  // NOLINT(build/include_order)
+#include <wpi/StringExtras.h>
+#include "wpinet/HttpParser.h"
+
+#include <iostream>
+
+
+namespace wpi {
+
+namespace uv = wpi::uv;
+
+class WebSocketTest {
+ public:
+  static const char* pipeName;
+  static const int port = 9002;
+
+  static void SetUpTestCase();
+
+  WebSocketTest() {
+    loop = uv::Loop::Create();
+    clientPipe = uv::Tcp::Create(loop);
+
+    auto failTimer = uv::Timer::Create(loop);
+    failTimer->timeout.connect([this] {
+      loop->Stop();
+      // FAIL() << "loop failed to terminate";
+    });
+    failTimer->Start(uv::Timer::Time{1000});
+    failTimer->Unreference();
+
+    resp.headersComplete.connect([this](bool) { headersDone = true; });
+
+    clientPipe->Connect(pipeName, port, [this]() {
+      clientPipe->StartRead();
+      clientPipe->data.connect([this](uv::Buffer& buf, size_t size) {
+        std::string_view data{buf.base, size};
+        if (!headersDone) {
+          data = resp.Execute(data);
+          if (resp.HasError()) {
+            Finish();
+          }
+          // ASSERT_EQ(resp.GetError(), HPE_OK)
+              // << http_errno_name(resp.GetError());
+          if (data.empty()) {
+            return;
+          }
+        }
+        std::cout << "DATA:\n";
+        for (auto i : data) {
+          std::cout << data;
+        }
+        std::cout << std::endl;
+        wireData.insert(wireData.end(), data.begin(), data.end());
+        if (handleData) {
+          handleData(data);
+        }
+      });
+      clientPipe->end.connect([this]() { Finish(); });
+    });
+  }
+
+  ~WebSocketTest() {
+    Finish();
+  }
+
+  void Finish() {
+    loop->Walk([](uv::Handle& it) { it.Close(); });
+  }
+
+  std::shared_ptr<uv::Loop> loop;
+  std::shared_ptr<uv::Tcp> clientPipe;
+  std::function<void()> setupWebSocket;
+  std::function<void(std::string_view)> handleData;
+  std::vector<uint8_t> wireData;
+  std::shared_ptr<WebSocket> ws;
+  HttpParser resp{HttpParser::kResponse};
+  bool headersDone = false;
+};
+
+
+const char* WebSocketTest::pipeName = "127.0.0.1";
+
+void WebSocketTest::SetUpTestCase() {
+// #ifndef _WIN32
+//   unlink(pipeName);
+// #endif
+}
+
+
+}  // namespace wpi
+
+int main() {
+  using namespace wpi;
+  WebSocketTest wsTest;
+
+  int gotCallback = 0;
+  std::string str = "Hello_tcp";
+  std::vector<uint8_t> data = {str.begin(), str.end()};
+  wsTest.setupWebSocket = [&] {
+    wsTest.ws->open.connect([&](std::string_view) {
+      wsTest.ws->SendText({{data}}, [&](auto bufs, uv::Error) {
+        ++gotCallback;
+        wsTest.ws->Terminate();
+        // ASSERT_FALSE(bufs.empty());
+        // ASSERT_EQ(bufs[0].base, reinterpret_cast<const char*>(data.data()));
+      });
+    });
+  };
+
+  wsTest.loop->Run();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+
+  // ASSERT_EQ(gotCallback, 1);
+  return (gotCallback == 1);
+}
+// int main() {}
--- src/MVP_Server.cpp
+++ src/MVP_Server.cpp
@@ -5,6 +5,7 @@
 #include "gtest/gtest.h"
 #include "wpinet/uv/Loop.h"
 #include "wpinet/uv/Pipe.h"
+#include "wpinet/uv/Tcp.h"
 #include "wpinet/uv/Timer.h"
 
 #include <wpinet/WebSocket.h>  // NOLINT(build/include_order)
@@ -21,22 +22,22 @@ namespace uv = wpi::uv;
 class WebSocketTest : public ::testing::Test {
  public:
   static const char* pipeName;
+  static const int port = 9002;
 
   static void SetUpTestCase();
 
   WebSocketTest() {
     loop = uv::Loop::Create();
-    clientPipe = uv::Pipe::Create(loop);
-    serverPipe = uv::Pipe::Create(loop);
+    serverPipe = uv::Tcp::Create(loop);
 
-    serverPipe->Bind(pipeName);
+    serverPipe->Bind(pipeName, 9002);
 
     auto failTimer = uv::Timer::Create(loop);
     failTimer->timeout.connect([this] {
       loop->Stop();
       FAIL() << "loop failed to terminate";
     });
-    failTimer->Start(uv::Timer::Time{1000});
+    failTimer->Start(uv::Timer::Time{20000});
     failTimer->Unreference();
 
     resp.headersComplete.connect([this](bool) { headersDone = true; });
@@ -47,32 +48,6 @@ class WebSocketTest : public ::testing::Test {
       if (setupWebSocket) {
         setupWebSocket();
       }
-    });
-    clientPipe->Connect(pipeName, [this]() {
-      clientPipe->StartRead();
-      clientPipe->data.connect([this](uv::Buffer& buf, size_t size) {
-        std::string_view data{buf.base, size};
-        if (!headersDone) {
-          data = resp.Execute(data);
-          if (resp.HasError()) {
-            Finish();
-          }
-          ASSERT_EQ(resp.GetError(), HPE_OK)
-              << http_errno_name(resp.GetError());
-          if (data.empty()) {
-            return;
-          }
-        }
-        for (auto i : data) {
-          std::cout << data;
-        }
-        std::cout << std::endl;
-        wireData.insert(wireData.end(), data.begin(), data.end());
-        if (handleData) {
-          handleData(data);
-        }
-      });
-      clientPipe->end.connect([this]() { Finish(); });
     });
   }
 
@@ -85,8 +60,8 @@ class WebSocketTest : public ::testing::Test {
   }
 
   std::shared_ptr<uv::Loop> loop;
-  std::shared_ptr<uv::Pipe> clientPipe;
-  std::shared_ptr<uv::Pipe> serverPipe;
+  std::shared_ptr<uv::Tcp> clientPipe;
+  std::shared_ptr<uv::Tcp> serverPipe;
   std::function<void()> setupWebSocket;
   std::function<void(std::string_view)> handleData;
   std::vector<uint8_t> wireData;
@@ -96,11 +71,12 @@ class WebSocketTest : public ::testing::Test {
 };
 
 
-#ifdef _WIN32
-const char* WebSocketTest::pipeName = "\\\\.\\pipe\\websocket-unit-test";
-#else
-const char* WebSocketTest::pipeName = "/tmp/websocket-unit-test";
-#endif
+// #ifdef _WIN32
+// const char* WebSocketTest::pipeName = "\\\\.\\pipe\\websocket-unit-test";
+// #else
+// const char* WebSocketTest::pipeName = "/tmp/websocket-unit-test";
+// #endif
+const char* WebSocketTest::pipeName = "127.0.0.1";
 
 void WebSocketTest::SetUpTestCase() {
 #ifndef _WIN32
@@ -110,7 +86,7 @@ void WebSocketTest::SetUpTestCase() {
 
 TEST_F(WebSocketTest, SendText) {
   int gotCallback = 0;
-  std::string str = "owo?";
+  std::string str = "Hello_tcp";
   std::vector<uint8_t> data = {str.begin(), str.end()};
   setupWebSocket = [&] {
     ws->open.connect([&](std::string_view) {
